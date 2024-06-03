66aa7f545266b553a62de0c592393f822258601d
Martial Saunois <cyb3rdad@protonmail.com>
2024-06-03 22:47:56 +0200

    Begin working on the response component

--- response.go
+++ response.go
@@ -5,4 +5,39 @@ import (
 	"net/http"
 )
 
+type HTTPSendable interface {
+	ContentType() string
+	Encode() ([]byte, error)
+	StatusCode() int
+}
+
+type HTTPResponse[View HTTPSendable, Writer http.ResponseWriter] struct {
+	headers              []string
+	genericInternalError []byte
+}
+
+// TODO: add tests
+type HTTPResponseOpts func(r *HTTPResponse[HTTPSendable, http.ResponseWriter])
+
+func WithHTTPResponseHeaders(headers []string) HTTPResponseOpts {
+	return func(r *HTTPResponse[HTTPSendable, http.ResponseWriter]) {
+		r.headers = headers
+	}
+}
+
+func NewHTTPResponse[View HTTPSendable, Writer http.ResponseWriter]() *HTTPResponse[View, Writer] {
+	return &HTTPResponse[View, Writer]{}
+}
+
+func (r *HTTPResponse[View, Writer]) Send(ctx context.Context, rw http.ResponseWriter, data View) {
+	rw.Header().Set("content-type", data.ContentType())
+	encoded, err := data.Encode()
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		rw.Write(r.genericInternalError)
+		return
+	}
+
+	rw.WriteHeader(data.StatusCode())
+	rw.Write(encoded)
 }
